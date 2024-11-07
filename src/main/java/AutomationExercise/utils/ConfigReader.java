16e211d4eb86caddb45ca2a3a9a9e08f0b2731d9
Ayush Sinha <ayushs@testvagrant.com>
2024-11-07 11:24:31 +0530

    Added ConfigReader file

---
+++ src/main/java/AutomationExercise/utils/ConfigReader.java
@@ -0,0 +1,14 @@
+package AutomationExercise.utils;
+
+public class ConfigReader {
+    private static final String baseURL = "https://automationexercise.com/";
+    private static final String browser = "chrome";
+
+    public static String getBrowser() {
+        return browser;
+    }
+
+    public static String getBaseURL() {
+        return baseURL;
+    }
+}
