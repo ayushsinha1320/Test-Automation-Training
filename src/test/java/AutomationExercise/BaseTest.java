9a31c54af1bd823568e7f0c946e32c5c06a72041
Ayush Sinha <ayushs@testvagrant.com>
2024-11-07 11:48:42 +0530

    feat(BaseTest): Added setup and tearDown methods
    Instantiated the driver and implemented setup() method to get the base URL for the test with waits and also implemented tearDown() to quit the driver.

--- src/test/java/AutomationExercise/BaseTest.java
+++ src/test/java/AutomationExercise/BaseTest.java
@@ -1,4 +1,23 @@
 package AutomationExercise;
 
+import AutomationExercise.shared.PageWaits;
+import AutomationExercise.utils.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
 public class BaseTest {
+    WebDriver driver = DriverCreator.instantiateDriver(ConfigReader.getBrowser());
+    PageWaits waits;
+
+    @BeforeSuite
+    public void setup(){
+        driver.get(ConfigReader.getBaseURL());
+        waits = PageWaits.getPageWaitsObject(driver);
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
 }
